f4ae5b2921541b3aaf1c236e9dbfd0f37cf134ee
DonTal-Code <Wissam.talebpro@gmail.com>
2021-05-25 23:14:49 +0200

    Add CategoryController, Two methods created (index and show) (quest8)

--- src/Controller/ProgramsController.php
+++ src/Controller/ProgramsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Program;
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,24 +14,43 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProgramsController extends AbstractController
 {
-    /**
-     * @Route("/", name="program_index")
-     */
-    public function index(): Response
+        /**
+         * Show all rows from Program’s entity
+         *
+         * @Route("/", name="index")
+         * @return Response A response instance
+         */
+        public function index(ProgramRepository $programRepository): Response
     {
-        return $this->render('programs/index.html.twig', [
-            'website' => 'Wild Series',
+        return $this->render(
+            'programs/index.html.twig',
+            ['programs' => $programRepository->findAll()]
+        );
+    }
+
+    /**
+     * Getting a program by id
+     *
+     * @Route("/show/{id<^[0-9]+$>}", name="show")
+     * @param int $id
+     * @return Response
+     */
+    public function show(int $id):Response
+    {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+        return $this->render('programs/show.html.twig', [
+            'program' => $program,
         ]);
     }
 
-    /**
-     * @Route("/{id}", requirements={"id"="\d+"}, name="show", methods={"GET"} )
-     */
-    public function show(int $id): Response
-    {
-        return $this->render('programs/show.html.twig', ['id' => 4]);
-
-    }
 
 
 }
