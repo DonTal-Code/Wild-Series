b57e63a38d068291ca4133cb59ebd04271757961
DonTal-Code <Wissam.talebpro@gmail.com>
2021-05-27 17:03:29 +0200

    Add Form Category and Program, Add View AddCategory and AddProgram , video in Readme for quest 12

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ https://www.loom.com/share/9f0029a9a9094e9fae14ca9822ddd29c
 The video link of my quest 11:
 https://www.loom.com/share/b76ad77c4b294766b61ec49d40ec1fa0
 
+The video link of my quest 12:
+https://www.loom.com/share/f9d28c63ee214ae3b77cf41a11b99bd8
+
--- src/Controller/ProgramsController.php
+++ src/Controller/ProgramsController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
 use App\Repository\ProgramRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,6 +30,36 @@ class ProgramsController extends AbstractController
             'programs/index.html.twig',
             ['programs' => $programRepository->findAll()]
         );
+    }
+
+    /**
+     * @Route ("/new", name="new")
+     * @param Request $request
+     * @return Response
+     */
+
+    public function new(Request $request): Response
+    {
+        $program = new Program();
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // For example : persiste & flush the entity
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($program);
+            $entityManager->flush();
+            // And redirect to a route that display the result
+            return $this->redirectToRoute('program_index');
+        }
+        return $this->render(
+            'programs/new.html.twig',
+            [
+                "form" => $form->createView(),
+            ]
+        );
+
     }
 
     /**
@@ -88,6 +120,8 @@ class ProgramsController extends AbstractController
 
         );
     }
+
+
 
     /**
      * @Route("/{programId<^[0-9]+$>}/seasons/{seasonId<^[0-9]+$>}/episodes/{episodeId<^[0-9]+$>}",  name="episode_show")
